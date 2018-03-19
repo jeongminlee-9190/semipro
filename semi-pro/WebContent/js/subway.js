6878b8f51e0c001cad6c3ca2667da2ad57b22a44
han_148 <han_148@naver.com>
2018-03-19 18:26:04 +0900

    subway js 수정 / fonts 추가

--- semi-pro/WebContent/js/subway.js
+++ semi-pro/WebContent/js/subway.js
@@ -1,31 +1,86 @@
 
 $(document).ready(function(){
-
+	
+	//button 클릭후 메뉴 보이기
 	$("#line_btn").click(function(){
-		$("#line").slideToggle("slow");
+		$("#line").slideToggle("fast");
 	});
 
 	$("#station_btn").click(function(){
-		$("#station_1").slideToggle("slow");
+		$("#station_1").slideToggle("fast");
 	});
-
-	/*$("#line_wrap ul li").hover(function(){
-		$("#line_wrap ul li").each(function(idx) {
-			var color_change = $("#line_wrap ul li span").css("background-color");
-			var bg_idx = $(this).index();
-			if(idx == bg_idx){
-				$(this).css({"background-color":color_change,"color":color_change});
-				$(this).find("span").css("background-color","#ffffff");
-			}
-			return idx != bg_idx;
-		});
-	});*/
 	
-	$("#line_wrap ul li").hover(function(){
-		var color_change = $("#line_wrap ul li span").css("background-color");
-
+	//메뉴 bg color 변경
+	//호선
+	$("#line_wrap ul li").mouseover(function(){
+		
+		var color_change = $(this).find("span").css("background-color");
+	
 		$(this).css({"background-color":color_change,"color":color_change});
 		$(this).find("span").css("background-color","#ffffff");
+
+	});
+	
+	$("#line_wrap ul li").mouseout(function(){
+		
+		var color = $(this).css("background-color");
+	
+		$(this).css({"background-color":"#ffffff","color":"#ffffff"});
+		$(this).find("span").css("background-color",color);
+
+	});
+	
+	//역
+	$("#station_wrap ul li").mouseover(function(){
+		
+		var color_change = $(this).css("color");
+	
+		$(this).css({"background-color":color_change,"color":"#ffffff"});
+
+	});
+	
+	$("#station_wrap ul li").mouseout(function(){
+		
+		var color = $(this).css("background-color");
+	
+		$(this).css({"background-color":"#ffffff","color":color});
+
+	});
+	
+	
+	//역 클릭 후 선택되기
+	$("#line_wrap ul li").click(function(idx){
+		
+		var color_change = $(this).css("background-color");
+		
+		var li = $(this);
+		$("#line_btn").html(function(){
+		    return "<span>" + $(li).find("span").text() + "</span>";
+		}) ;
+		
+		$("#line_btn").css({"background-color":color_change,"color":color_change});
+		$("#line_btn").find("span").css({"background-color":"#fff","display":"inline-block",
+										 "padding":"5px 10px","border-radius":"100px",
+										 "font-family":"gotham_Bold", 
+										 "font-style":"normal","font-weight":"normal","font-size":"21px"})
+		
+		//1호선 css 따로 수정
+		if( $(li).find("span").text() == 1 ){
+			$("#line_btn").find("span").css({"background-color":"#fff","display":"inline-block",
+											 "padding":"5px 13px","border-radius":"100px",
+											 "font-family":"gotham_Bold", 
+											 "font-style":"normal","font-weight":"normal","font-size":"21px"})
+		}
+		
+		//한글부분 수정
+		if( $(li).index() >= 9 ){
+			$("#line_btn").find("span").css({"background-color":"#fff","display":"inline-block",
+											 "padding":"2px 11px 6px 11px","border-radius":"25px",
+											 "font-size":"18px","font-weight":"900", "font-weight":"900",
+											 "font-family":"'Open Sans', sans-serif"})
+		}
+		console.log($(li).index());
+		$("#line").slideUp("fast");
 		
 	});
 	
